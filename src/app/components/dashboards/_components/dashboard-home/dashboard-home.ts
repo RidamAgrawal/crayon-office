f3d14c1272b21a8623b89c45ebb1a30110c28e23
RidamAgrawal <agarwalagam04@gmail.com>
2026-05-04 00:00:40 +0530

    changes: fetch spaces integration on home

--- src/app/components/dashboards/_components/dashboard-home/dashboard-home.ts
+++ src/app/components/dashboards/_components/dashboard-home/dashboard-home.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { TabComponent } from '../tabs/tabs';
 import { TabularTemplate1 } from '../../../../templates/tabular-template-1/tabular-template-1';
 import { TabTemplate2 } from '../../../../templates/tab-template-2/tab-template-2';
+import { HttpService } from '../../../../services/http-service/http-service';
 
 @Component({
   selector: 'app-dashboard-home',
   imports: [TabComponent, TabularTemplate1, TabTemplate2],
   templateUrl: './dashboard-home.html',
-  styleUrl: './dashboard-home.scss'
+  styleUrl: './dashboard-home.scss',
 })
-export class DashboardHome {
+export class DashboardHome implements OnInit {
+  private readonly httpService = inject(HttpService);
+  protected readonly spaces: WritableSignal<any[]> = signal<any>([]);
 
+  public ngOnInit(): void {
+    this.httpService.getSpaces().subscribe((response) => {
+      this.spaces.set(response);
+    });
+  }
 }
